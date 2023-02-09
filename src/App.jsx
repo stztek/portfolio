8e1e5a33d4f503a2881094fdf21ddfaa8a11685b
stztek <sairatzmedia@gmail.com>
2023-02-09 16:59:20 -0300

    mi portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,37 @@ import "./css/App.css";
 import Main from "./mainContainer.jsx";
 import Nav from "./nav.jsx";
 import Footer from "./footer";
-
+import AdminPanel from "./components/admincont";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const About = lazy(() => import("./about.jsx"));
 const Contact = lazy(() => import("./contact.jsx"));
 function App() {
   return (
     <div>
-      <Nav />
-      <Main />
-      <Suspense
-        fallback={
-          <div className="loading-center">
-            <div className="loading"></div>
-          </div>
-        }
-      >
-        <About />
-        <Contact />
-      </Suspense>
+      <Nav/>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/" element={<Main/>} />
+          <Route path="/admin-panel-portfolio" element={<AdminPanel/>} />
+
+   
+        </Routes>
+
+        <Suspense
+          fallback={
+            <div className="loading-center">
+              <div className="loading"></div>
+            </div>
+          }
+        >
+          <About />
+          <Contact />
+        </Suspense>
+
+      </BrowserRouter>
+      
       <Footer />
     </div>
   );
